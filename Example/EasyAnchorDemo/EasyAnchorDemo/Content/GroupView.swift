e2285a7e2d0e43a5e32cba67a9c34ed0f271b8a4
zhangzhongfu <xtaykhksghy108@outlook.com>
2023-07-28 00:26:56 +0800

    原demo不可用，修改为可用demo

---
+++ Example/EasyAnchorDemo/EasyAnchorDemo/Content/GroupView.swift
@@ -0,0 +1,58 @@
+import UIKit
+import EasyAnchor
+
+class GroupView: UIView {
+  let container = View(text: "", color: Color.container)
+  let a = View(text: "a", color: Color.color1)
+  var animator: Animator!
+  var g1: Group!
+  var g2: Group!
+  var g3: Group!
+  var g4: Group!
+
+  override func didMoveToSuperview() {
+    super.didMoveToSuperview()
+
+    addSubview(container)
+    container.addSubview(a)
+
+      EasyAnchor.activate(
+      container.anchor.edges.insets(8),
+      a.anchor.size.equal.to(60)
+    )
+
+    g1 = group(a.anchor.top.left)
+    g2 = group(a.anchor.top.right)
+    g3 = group(a.anchor.bottom.right)
+    g4 = group(a.anchor.bottom.left)
+
+    g1.isActive = true
+
+    animator = Animator(view: self, animations: [
+      {
+        self.toggle(self.g2)
+      },
+      {
+        self.toggle(self.g3)
+      },
+      {
+        self.toggle(self.g4)
+      },
+      {
+        self.toggle(self.g1)
+      }
+      ])
+
+    animator.start()
+  }
+
+  func toggle(_ group: Group) {
+    [g1, g2, g3, g4].forEach { g in
+      guard let g = g else {
+        return
+      }
+
+      g.isActive = (g == group)
+    }
+  }
+}
---
+++ Example/EasyAnchorDemo/EasyAnchorDemo/ContentController.swift
@@ -0,0 +1,29 @@
+import UIKit
+import EasyAnchor
+
+class ContentController: UIViewController {
+  var content: ListController.Content!
+  var contentView: UIView!
+  let container = UIView()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.backgroundColor = .white
+    title = content.title
+
+    automaticallyAdjustsScrollViewInsets = false
+
+    view.addSubview(container)
+    container.backgroundColor = UIColor(hex: "ecf0f1")
+    activate(
+      container.anchor.center,
+      container.anchor.width.equal.to(300),
+      container.anchor.height.equal.to(container.anchor.width)
+    )
+
+    contentView = content.view.init()
+    container.addSubview(contentView)
+    activate(contentView.anchor.edges)
+  }
+}
