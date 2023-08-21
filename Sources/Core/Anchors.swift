0a34136f881ca7c1e9f2cf01ea8908f5f8aa9679
zhangzhongfu <xtaykhksghy108@outlook.com>
2023-08-22 06:38:04 +0800

    新增： 支持 either、optional

--- Sources/Core/Anchors.swift
+++ Sources/Core/Anchors.swift
@@ -10,15 +10,44 @@ public func group(_ producers: ConstraintProducer ...) -> Group {
 }
 
 /// Produce constraints into group and activate
-public func activate(@ActivateBuilder contents: () -> Void) -> Void {
+public func activate(@ActivateBuilder contents: () -> Void) {
     contents()
 }
 
 @resultBuilder
 public struct ActivateBuilder {
-    public static func buildBlock(_ components: ConstraintProducer...) -> Void {
-        let group = Group(producers: components)
+    public static func buildPartialBlock(first: [ConstraintProducer]) -> [ConstraintProducer] {
+        first
+    }
+    
+    public static func buildPartialBlock(accumulated: [ConstraintProducer], next: [ConstraintProducer]) -> [ConstraintProducer] {
+        if next.isEmpty {
+            return accumulated
+        } else {
+            var array = accumulated
+            array.append(next[0])
+            return array
+        }
+    }
+    
+    public static func buildEither(first component: [ConstraintProducer]) -> [ConstraintProducer] {
+        component
+    }
+    
+    public static func buildEither(second component: [ConstraintProducer]) -> [ConstraintProducer] {
+        component
+    }
+    
+    public static func buildOptional(_ component: [ConstraintProducer]?) -> [ConstraintProducer] {
+        component ?? []
+    }
+    
+    public static func buildExpression(_ expression: ConstraintProducer) -> [ConstraintProducer] {
+        [expression]
+    }
+    
+    public static func buildFinalResult(_ component: [ConstraintProducer]) {
+        let group = Group(producers: component)
         group.isActive = true
-        return ()
     }
 }
